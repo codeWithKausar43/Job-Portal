0e612420f77cb9cac68e7c59e317dd27ab901397
kausar ahammed <kausar.a.pro@gmail.com>
2024-12-10 23:18:25 +0600

    adder authProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './router/router'
+import AuthProvider from './contex/AuthProvider'
  
  
  
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <RouterProvider router={router}></RouterProvider>
+  <AuthProvider>
+  <RouterProvider router={router}></RouterProvider>
+  </AuthProvider>
   </StrictMode>,
 )
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,29 +1,48 @@
+import Lottie from "lottie-react";
+import registerLottieData  from "../../assets/register.json"
+import { useContext } from "react";
+import AuthContext from "../../contex/AuthContext";
+ 
  
 
 const Register = () => {
+  const {createUser} = useContext(AuthContext)
+const handleFormRegister = e => {
+    e.preventDefault()
+    const form = e.target 
+    const email  = form.email.value;
+    const password = form.password.value;
+    console.log(email, password)
+    //validation korte hobe
+    createUser(email, password)
+    .then(result => {
+      console.log(result.user)
+    })
+    .catch(error => {
+      console.log(error.message)
+    })
+}
+    
     return (
         <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
-        
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-            quasi. In deleniti eaque aut repudiandae et a id nisi.
-          </p>
+        <div className="text-center lg:text-left w-96">
+        <Lottie animationData={registerLottieData}></Lottie>
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <form className="card-body">
+          <form  onSubmit={handleFormRegister} className="card-body">
           <h1 className="text-3xl text-center font-bold">Login now!</h1>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
-              <input type="email" placeholder="email" className="input input-bordered" required />
+
+              <input name="email" type="email"  placeholder="email" className="input input-bordered" required />
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
-              <input type="password" placeholder="password" className="input input-bordered" required />
+              <input type="password" name="password" placeholder="password" className="input input-bordered" required />
               <label className="label">
                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
               </label>
